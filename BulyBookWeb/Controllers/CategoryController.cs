3de4c9201f09fe3a87c11d7943e49bf01bf650db
Samit Paudel <samitpdl@gmail.com>
2022-07-08 12:19:58 +0545

    Complete solution updated

---
+++ BulyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using BulyBookWeb.Data;
+using BulyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BulyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        // GET:
+        public IActionResult Create()
+        { 
+            return View();
+        }
+
+        // POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category added successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET:
+        public IActionResult Edit(int? id)
+        {
+            if(id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDb = _db.Categories.SingleOrDefault(u=>u.Id == id);
+            return View(categoryFromDb);
+        }
+
+        // POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        // GET:
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDb = _db.Categories.SingleOrDefault(u=>u.Id == id);
+            return View(categoryFromDb);
+        }
+
+        // POST:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category deleted successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ BulyBookWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BulyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulyBookWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set;}
+    }
+}
